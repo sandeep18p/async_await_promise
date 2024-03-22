4a9964f5ca6997905d96c29096d0e5e5f2daf83b
sandeep18p <sandeep.pansari98@gmail.com>
2024-03-22 18:09:50 +0530

    new

---
+++ 1/index.js
@@ -0,0 +1,19 @@
+async function fetchData() {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+      throw error;
+    }
+  }
+  
+  // Call the async function
+  fetchData();
+  
+    console.log("hello")
